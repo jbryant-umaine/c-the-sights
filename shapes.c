8c893531bb7512c4c430325c381b170311a3d2a5
Jacob Bryant <jacob.bryant1@maine.edu>
2024-09-10 22:17:12 -0400

    Implemented createPyramidReverse function

--- shapes.c
+++ shapes.c
@@ -1,3 +1,9 @@
+/**
+ * Date: 9/10/2024
+ *
+ * Author: Jacob Bryant
+ * Class: COS 135
+ */
 #include <stdio.h>
 
 void createPyramid(int number)
@@ -33,10 +39,43 @@ void createPyramid(int number)
     }
 }
 
+void createPyramidReverse(int number)
+{
+    int rows;
+    int stars = (2 * number) - 1; // Start with largest row
+    int spaces = 0;               // Start with no leading spaces
+
+    // Iterate many of times defined in "number"
+    for (rows = 0; rows < number; rows++)
+    {
+
+        // Padding in front of the stars
+        int i;
+        for (i = spaces; i > 0; i--)
+        {
+            printf(" ");
+        }
+
+        // Add stars to pyramid
+        for (i = 0; i < stars; i++)
+        {
+            printf("x");
+        }
+
+        // Decrement star count to an odd number for each row
+        stars -= 2;
+
+        // Increase the number of spaces required for padding
+        spaces++;
+
+        printf("\n");
+    }
+}
+
 int main()
 {
-    createPyramid(3);
+    createPyramid(7);
     printf("\n");
-    createPyramid(5);
+    createPyramidReverse(5);
     return 0;
 }
