00b7f3fcc6d3bf8b372f460fc91938f49ac26ab6
jbryant-umaine <jacob.bryant1@maine.edu>
2024-10-01 18:46:54 +0000

    Add room factory for dynamic Room creation and begin line-reading logic

--- dungeonmaster.c
+++ dungeonmaster.c
@@ -1,12 +1,25 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 struct Room
 {
-    char name;
-    char code;
-    char description;
+    char name[50];
+    char code[2];
+    char description[200];
 };
+
+struct Room *createRoom(
+    char *name,
+    char *code,
+    char *description)
+{
+    struct Room *x = malloc(sizeof(struct Room));
+    strcpy(x->name, name);
+    strcpy(x->code, code);
+    strcpy(x->description, description);
+    return x;
+}
 
 int main(int argc, char *argv[])
 {
@@ -16,13 +29,24 @@ int main(int argc, char *argv[])
         return 0;
     }
 
+    // struct Room rooms[1000];
+
     FILE *fp = fopen(argv[1], "r");
 
     char line[2048];
 
+    int roomCount = 0;
+    int roomProperty = 0;
     while (fgets(line, sizeof(line), fp) != NULL)
     {
-        printf("Line: %s", line);
+
+        if (strlen(line) != 0)
+        {
+            printf("Line: %s", line);
+        }
+
+        roomProperty++;
+        roomCount++;
     }
 
     fclose(fp);
