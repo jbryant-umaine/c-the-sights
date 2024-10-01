9f44e57913b6e9f5a45822b71bae92f2f7342bde
jbryant-umaine <jacob.bryant1@maine.edu>
2024-10-01 20:16:53 +0000

    Finalize program with output of LinkedList with room nodes

--- dungeonmaster.c
+++ dungeonmaster.c
@@ -1,25 +1,42 @@
+/**
+ * Date: 10/1/2024
+ *
+ * Author: Jacob Bryant
+ * Class: COS 135
+ */
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+// Room struct that defines required data.
 struct Room
 {
     char name[50];
-    char code[2];
-    char description[200];
+    char code[3];
+    char description[1000];
 };
 
+// Factory for dynamically creating a room struct.
 struct Room *createRoom(
     char *name,
     char *code,
     char *description)
 {
     struct Room *x = malloc(sizeof(struct Room));
-    strcpy(x->name, name);
-    strcpy(x->code, code);
-    strcpy(x->description, description);
+    // Copy strings into the struct fields and null-terminate them.
+    snprintf(x->name, sizeof(x->name), "%s", name);
+    snprintf(x->code, sizeof(x->code), "%s", code);
+    snprintf(x->description, sizeof(x->description), "%s", description);
     return x;
 }
+
+// LinkedList implementation
+typedef struct NODE
+{
+    struct Room *data;
+    struct NODE *next;
+} NODE;
 
 int main(int argc, char *argv[])
 {
@@ -29,24 +46,91 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    // struct Room rooms[1000];
-
+    // Open file
     FILE *fp = fopen(argv[1], "r");
 
-    char line[2048];
+    char line[50000];
 
     int roomCount = 0;
     int roomProperty = 0;
+
+    char name[50], code[3], description[1000];
+
+    NODE *head = NULL;
+    NODE *p = NULL;
+
+    // Read file line by line
     while (fgets(line, sizeof(line), fp) != NULL)
     {
-
-        if (strlen(line) != 0)
+        // Room name
+        if (roomProperty == 0)
         {
-            printf("Line: %s", line);
+            // Copy room name into temp name buffer.
+            sscanf(line + 11, "%[^\n]", name);
+        }
+
+        // Room code
+        if (roomProperty == 1)
+        {
+            // Copy room code into temp code buffer.
+            sscanf(line + 11, "%s", code);
+        }
+
+        // Room desc
+        if (roomProperty == 2)
+        {
+            // Copy room desc into temp desc buffer.
+            sscanf(line + 18, "%[^\n]", description);
         }
 
         roomProperty++;
-        roomCount++;
+
+        // No more room properties to read.
+        if (roomProperty == 4)
+        {
+            // Create a new room
+            struct Room *newRoom = createRoom(name, code, description);
+
+            // Create a new node
+            NODE *newNode = malloc(sizeof(NODE));
+
+            // Set node data to new node and set next to NULL.
+            newNode->data = newRoom;
+            newNode->next = NULL;
+
+            // Set current head to this node.
+            if (head == NULL)
+            {
+                head = newNode;
+            }
+            else // Set latest node's next to this node.
+            {
+                p->next = newNode;
+            }
+
+            // Set node pointer to the latest node.
+            p = newNode;
+
+            roomCount++;
+            roomProperty = 0;
+
+            // Reset string allocations.
+            memset(name, 0, sizeof(name));
+            memset(code, 0, sizeof(code));
+            memset(description, 0, sizeof(description));
+        }
+    }
+
+    NODE *current = head;
+
+    // Traverse linked list and print out room data.
+    while (current != NULL)
+    {
+        if (current->data != NULL)
+        {
+            printf("Room %s: %s\n", current->data->code, current->data->name);
+        }
+        current = current->next;
     }
 
     fclose(fp);
